c881dea9cdb12359b2d8afdc586a4b76ccd68f0b
EXLCORP\assafm <assaf.mozes@exlibrisgroup.com>
2021-05-06 14:13:00 +0300

    Initial commit

---
+++ src/main/java/ConectLayerAPI.java
@@ -0,0 +1,88 @@
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
+import kong.unirest.json.JSONArray;
+import kong.unirest.json.JSONObject;
+import org.apache.commons.io.IOUtils;
+import org.apache.log4j.Logger;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.parser.Parser;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ConectLayerAPI {
+    private final static String CCL_HOST = "https://idp.ombiel.co.uk";
+    private final static String CCL_ENDPOINT_WSDL = "/demo/services/CampusMUniversityService/";
+    private final static String CCL_BASIC_AUTH_USER = "application_sec_user";
+    private final static String CCL_BASIC_AUTH_PASS = "connect";
+    private final static List<String> eventsTypes = Arrays.asList("TT", "ET", "UE", "ST");
+    private final static String USERNAME =  "MultiDevice%s";
+    private final static String EMAIL_PREFIX =  "@exlibrisgroup.com";
+    private final static String EMAIL =  USERNAME + EMAIL_PREFIX;
+    private final static String STUDENT_SSO = "hannan.abunamous" + EMAIL_PREFIX;
+    private final static String STUDENT_LDAP = "hannan";
+
+    private final static String PASSWORD =  "campusm";
+    private final static int USER_BASE = 322;
+    public static ZoneId zoneIsr = ZoneId.of("Israel");
+
+    private static final Logger log = Logger.getLogger(ConectLayerAPI.class);
+    private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXX");
+    private static DateTimeFormatter formatterDay = DateTimeFormatter.ofPattern("e");
+
+    public static void main(String[] args) {
+        createSimpleEvent("MultiDevice1");
+    }
+
+
+
+    private static void createSimpleEvent(String userName) {
+        LocalTime midnight = LocalTime.MIDNIGHT;
+        LocalDate today = LocalDate.now(zoneIsr);
+        JSONObject event = readFromJsonFileToObject("src/main/resources/eventsOverlapFull.json").getJSONArray("events").getJSONObject(0);
+        Map<String, Object> parameters = event.toMap();
+        parameters.compute("start", (key, val) ->
+            ZonedDateTime
+                .of(LocalDateTime.of(today, midnight)
+                    .withHour(Integer.parseInt((String)val)), zoneIsr)
+                .format(formatter));
+        String parametersString =   parameters.entrySet()
+            .stream()
+            .map(entry -> "&" + entry.getKey() + "=" + entry.getValue())
+            .collect(Collectors.joining());
+        String payload =  String.format("createTimetableEntry?username=%s&password=%s",
+            userName, PASSWORD);
+        String URL = CCL_HOST + CCL_ENDPOINT_WSDL + payload + parametersString;
+        HttpResponse<String> res = Unirest.get(URL)
+            .basicAuth(CCL_BASIC_AUTH_USER, CCL_BASIC_AUTH_PASS)
+            .asString();
+        Document doc = Jsoup.parse(res.getBody(), "", Parser.xmlParser());
+        System.out.println("creating event for :" + userName + ": " + res.getStatus() + " " + doc.select("ns1|desc").text());
+    }
+
+    public static JSONObject readFromJsonFileToObject(String path) {
+        try {
+            if (!(new File(path)).exists()) {
+                throw new Exception("no such file: " + path );
+            }
+            InputStream is = new FileInputStream(path);
+            return new JSONObject(IOUtils.toString(is, "UTF-8"));
+        } catch (Exception e) {
+            try {
+                throw new Exception("unable to read json from file: " + e );
+            } catch (Exception ex) {
+            }
+        }
+        return null;
+    }
+
+}
